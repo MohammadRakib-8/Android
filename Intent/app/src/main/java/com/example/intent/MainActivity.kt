17b60357ce60255cc6daa7a86c32df273e4821d1
Moahmmad Rakib <mohammadrakib224@gmail.com>
2025-08-05 12:55:07 +0600

    Intent Passing

---
+++ Intent/app/src/main/java/com/example/intent/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.example.intent
+
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var clickMeBTN: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        clickMeBTN=findViewById(R.id.clickMeBTN)
+
+ clickMeBTN.setOnClickListener {
+Log.v("bugtsdt","Button Clicked")
+     val intent = Intent(
+         this, Second_activity2::class.java)
+                 startActivity(intent)
+ }
+    }
+}
